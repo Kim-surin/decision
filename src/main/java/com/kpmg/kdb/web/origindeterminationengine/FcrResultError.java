f55643721c65df80631dd81b1b2ca02c2d3bcf43
surin <kimsur1111@naver.com>
2026-09-18 14:11:41 +0900

    Merge branch 'decision/master_0917' of https://github.com/Kim-surin/decision.git into decision/master_0917

--- src/main/java/com/kpmg/kdb/web/origindeterminationengine/FcrResultError.java
+++ src/main/java/com/kpmg/kdb/web/origindeterminationengine/FcrResultError.java
@@ -8,19 +8,17 @@ import com.kpmg.kdb.web.origindeterminationengine.dto.OriginDeterminationResult;
 public enum FcrResultError {
 
 	/** 해당 HS코드에 적용 가능한 룰이 전혀 없는 경우 */
-	STANDARD_NOT_EXIST("MSG_DECISION_STANDARD_NOT_EXIST", "해당 협정의 제품 HS코드에 해당하는 판정 기준(PSR)이 존재하지 않습니다."),
+	STANDARD_NOT_EXIST("MSG_DECISION_STANDARD_NOT_EXIST", "해당 협정의 품목 HS코드에 해당하는 판정 기준(PSR)이 존재하지 않습니다"),
 	/** RVC_CTC 모드에서 재료비(역내+역외)가 0원인 경우 */
-	QTY_AMOUNT_ZERO("MSG_FAILED_DECISION_QTY_AMOUNT", "재료비(역내+역외) 합계가 0인 품목이 존재합니다."),
-	/** RVC 판정 기준금액(FOB/EXW 또는 순원가)이 0 이하인 경우. QTY_AMOUNT_ZERO와 원래 코드를 공유했으나,
-	 * GET_CODE_NAME('ODER') 다국어 조회가 등록돼 있으면 서로 다른 메시지가 묻혀버려 별도 코드로 분리함. */
-	RVC_BASE_AMOUNT_NOT_POSITIVE("MSG_RVC_BASE_AMOUNT_NOT_POSITIVE",
-			"RVC 판정 기준금액(FOB/EXW 또는 순원가)이 0 이하여서 비율을 계산할 수 없습니다."),
+	QTY_AMOUNT_ZERO("MSG_FAILED_DECISION_QTY_AMOUNT", "재료비가 0인 자재가 존재합니다"),
+	/** RVC 판정 기준금액(FOB/EXW 또는 순원가)이 0 이하인 경우 */
+	RVC_BASE_AMOUNT_NOT_POSITIVE("MSG_RVC_BASE_AMOUNT_NOT_POSITIVE","RVC 판정 기준금액(FOB/EXW 또는 순원가)이 0 이하여서 비율을 계산할 수 없습니다"),
 	/** CTC 판정 대상 자재의 HS코드가 누락된 경우 */
-	HSCODE_INCLUDE_MISSING("TXT_HSCODE_INCLUDE_MISSING", "HS 코드가 누락된 자재가 존재합니다."),
+	HSCODE_INCLUDE_MISSING("TXT_HSCODE_INCLUDE_MISSING", "HS코드가 누락된 자재가 존재합니다"),
 	/** RVC 판정에 필요한 제품 중량 정보를 못 찾은 경우 */
-	PRODUCT_WEIGHT_NOT_FOUND("MSG_PRODUCT_WEIGHT_NOT_FOUND", "미소기준(중량기준) 판정에 필요한 제품 중량 정보가 존재하지 않습니다."),
+	PRODUCT_WEIGHT_NOT_FOUND("MSG_PRODUCT_WEIGHT_NOT_FOUND", "미소기준(중량기준) 판정에 필요한 품목의 중량 정보가 존재하지 않습니다"),
 	/** CREATE_FCR 단계에서 실적/표준 BOM을 모두 못 찾은 경우 */
-	BOM_NOT_FOUND("BOM_NOT_FOUND", "실적 BOM과 표준 BOM을 모두 찾을 수 없습니다."),
+	BOM_NOT_FOUND("BOM_NOT_FOUND", "품목의 BOM을 찾을 수 없습니다"),
 	/** UPDATE_FRM 재조회 결과 역내/역외산 어느 쪽도 매칭되지 않은 경우 */
 	ALL_ERROR("ALL-ERROR", null),
 	/** loadBuffer 실패(버퍼율 조회 실패) */
